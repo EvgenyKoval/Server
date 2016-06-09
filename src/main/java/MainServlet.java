7f9335ddf37f0da748226f9086f24f2ca3023d1c
EvgenyKoval <shiller1993@gmail.com>
2016-06-10 02:45:01 +0300

    add directories

---
+++ src/main/java/MainServlet.java
@@ -0,0 +1,40 @@
+import com.sun.xml.internal.messaging.saaj.util.Base64;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ragvalod on 10.06.16.
+ */
+public class MainServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String partNumber = request.getParameter("number");
+        String hash = request.getParameter("hash");
+        String data = request.getParameter("data");
+        Boolean isLast = Boolean.valueOf(request.getParameter("isLast"));
+
+        data = Base64.base64Decode(data);
+        System.out.println(data);
+
+        if (isLast){
+            System.out.println("isLast:" + isLast);
+            Saver saver = new Saver(partNumber, data, isLast);
+            saver.savePart();
+        }
+        else if (true/*hash.equals(DigestUtils.md5Hex(data))*/) {
+            Saver saver = new Saver(partNumber, data);
+            saver.savePart();
+        } else {
+            response.sendError(400, "REPEAT");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("<h1>use POST method</h1>");
+    }
+}
---
+++ src/main/java/Saver.java
@@ -0,0 +1,68 @@
+import java.io.*;
+
+/**
+ * Created by ragvalod on 10.06.16.
+ */
+public class Saver {
+    private String name;
+    private String data;
+    private static int partCount = 0;
+    private static boolean isLast = false;
+
+
+    public Saver(String name, String data) {
+        this.name = name;
+        this.data = data;
+    }
+
+    public Saver(String name, String data, boolean isLast) {
+        this.name = name;
+        this.data = data;
+        this.isLast = isLast;
+        partCount = Integer.valueOf(name);
+
+    }
+
+    public void savePart() {
+        System.out.println("in savepart method");
+        try {
+            File file = new File("../work/" + name);
+            FileWriter fileWriter = new FileWriter(file);
+            if (file.canWrite()) {
+                fileWriter.write(data);
+                fileWriter.flush();
+                fileWriter.close();
+                System.out.println("file was create");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (isLast) {
+            try {
+                makeFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void makeFile() throws Exception {
+        System.out.println("in makeFile method");
+        File dir = new File("../work/");
+        File[] files = dir.listFiles();
+        System.out.println("dir contain " + files.length + " files");
+        if (files.length == partCount) {
+            File finalFile = new File("../work/Final");
+            System.out.println(finalFile.getAbsolutePath());
+            FileWriter fileWriter = new FileWriter(finalFile);
+            BufferedReader reader;
+            for (File file : files) {
+                reader = new BufferedReader(new FileReader(file));
+                fileWriter.write(reader.readLine());
+                reader.close();
+            }
+            fileWriter.flush();
+            fileWriter.close();
+        }
+    }
+}
